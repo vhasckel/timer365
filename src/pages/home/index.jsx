c5133059de599498ef26a5da143fbd71dcdcb271
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-09-05 20:50:50 -0300

    fix: alinhamento do conteúdo da página home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Timer from "../../components/timer";
 import NewCycle from "../../components/new-cycle";
+import "./home.css";
 
 export default function HomePage() {
   return (
-    <div>
+    <div className="container--home">
       <NewCycle />
       <Timer />
     </div>
