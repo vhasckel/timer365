d4e9dcaad2fa5ff2e826bef5c1277670c8a36555
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-09-05 20:47:24 -0300

    feat: estrutura componente new-cycle

---
+++ src/components/new-cycle/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./new-cycle.css";
+
+export default function NewCycle() {
+  return (
+    <div className="container--new-cycle">
+      <label htmlFor="task">Vou trabalhar em</label>
+      <input type="text" id="task" placeholder="Criar timer365..." />
+
+      <label htmlFor="minutesAmount">durante</label>
+      <input type="number" id="minutesAmount" />
+
+      <span>minutos.</span>
+    </div>
+  );
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Timer from "../../components/timer";
+import NewCycle from "../../components/new-cycle";
 
 export default function HomePage() {
   return (
     <div>
+      <NewCycle />
       <Timer />
     </div>
   );
