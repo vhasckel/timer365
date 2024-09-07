25980e3a31cd57abe56631aea572b212fce61918
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-09-07 13:53:33 -0300

    feat: countdown visual do timer

--- src/components/new-cycle/index.jsx
+++ src/components/new-cycle/index.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import "./new-cycle.css";
+import { useFormContext } from "react-hook-form";
 
 export default function NewCycle() {
+  const { register } = useFormContext();
   return (
     <div className="container--new-cycle">
       <label htmlFor="task">Vou trabalhar em</label>
-      <input type="text" id="task" placeholder="Criar timer365..." />
+      <input
+        type="text"
+        id="task"
+        placeholder="Criar timer365..."
+        {...register("task")}
+      />
 
       <label htmlFor="minutesAmount">durante</label>
-      <input type="number" id="minutesAmount" />
+      <input
+        type="number"
+        id="minutesAmount"
+        {...register("minutesAmount", { valueAsNumber: true })}
+      />
 
       <span>minutos.</span>
     </div>
